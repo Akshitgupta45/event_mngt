5a8b6ece1527bd55d58cc31abe4aafa797ea67fd
Akshitgupta45 <akshit.rke@gmail.com>
2024-01-18 08:19:49 +0530

    get method added with listing event

--- router/event.js
+++ router/event.js
@@ -3,7 +3,7 @@ const eventController = require("../controllers/event");
 const router = new express.Router();
 
 router.post("/create", eventController.createEvent);
-router.post("/list", eventController.getEvent);
+router.get("/list", eventController.getEvent);
 router.post("/join", eventController.joinEvent);
 
 module.exports = router;
